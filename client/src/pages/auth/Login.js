a63e40112ab0b0efb4d8a4f2d069b709695fe213
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-02 11:17:25 -0600

    implement login with google

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import { auth } from '../../firebase';
+import { auth, googleAuthProvider } from '../../firebase';
 import { toast } from 'react-toastify'
 import { Button } from 'antd'
-import { MailOutlined } from "@ant-design/icons";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch } from 'react-redux'
 
 
@@ -37,6 +37,26 @@ const Login = ({ history }) => {
         }
     }
 
+    const googleLogin = async () => {
+        auth.signInWithPopup(googleAuthProvider)
+            .then(async (result) => {
+                const { user } = result
+                const idTokenResult = await user.getIdTokenResult()
+                dispatch({
+                    type: 'LOGGED_IN_USER',
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token
+                    }
+                })
+                history.push('/')
+            })
+            .catch((err) => {
+                console.log(err);
+                toast.error(err.message);
+            });
+    }
+
     const loginForm = () => <form onSubmit={handleSubmit}>
         <div className='form-group'>
             <input type='email' className='form-control' value={email}
@@ -55,8 +75,10 @@ const Login = ({ history }) => {
         <div className='container p-5'>
             <div className='row'>
                 <div className='col-md-6 offset-md-3'>
-                    <h4>Login</h4>
+                    {!loading ? (<h4>Login</h4>) : (<h4 className='text-danger'>Loading...</h4>)}
                     {loginForm()}
+                    <Button onClick={googleLogin} type='danger' className='mb-3' block shape='round'
+                        icon={<GoogleOutlined />} size='large'>Login with Google</Button>
                 </div>
             </div>
         </div>
