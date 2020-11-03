e9e87c85316d853c2708e80c226db18e53bd7dc1
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-03 11:33:58 -0600

    implement controllers

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//import 
+const { createOrUpdateUser } = require('../controllers/auth')
 
-router.get('/create-or-update-user', (req, res) => {
-    res.json({
-        data: 'You hit create-or-update-user API endpoint'
-    })
-})
+router.get('/create-or-update-user', createOrUpdateUser)
 module.exports = router;
