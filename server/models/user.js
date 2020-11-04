0bc21decd20bf4b3a8c2e8503c58e73e71a3d2bb
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-04 09:59:24 -0600

    implement create or update user

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,26 @@
-const mongoose = require('mongoose')
-const { ObjectId } = mongoose.Schema
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 
-const userSchema = new mongoose(
+const userSchema = new mongoose.Schema(
     {
         name: String,
         email: {
             type: String,
             required: true,
-            index: true
+            index: true,
         },
         role: {
             type: String,
-            default: 'subscriber'
+            default: "subscriber",
         },
         cart: {
             type: Array,
-            default: []
+            default: [],
         },
         address: String,
-        // wishlist: [{ type: ObjectId, ref: 'Product' }]
-    }, { timestamps: true })
+        //   wishlist: [{ type: ObjectId, ref: "Product" }],
+    },
+    { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
