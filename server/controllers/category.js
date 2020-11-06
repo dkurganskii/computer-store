b4ef7ea72828cfc4054dd1a6b0f13541677bb03b
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-06 15:36:10 -0600

    add list, read, update and delete category controllers

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,5 +1,6 @@
 const Category = require('../models/category');
-const slugify = require('slugify')
+const slugify = require('slugify');
+const category = require('../models/category');
 
 
 exports.create = async (req, res) => {
@@ -13,17 +14,31 @@ exports.create = async (req, res) => {
 }
 
 exports.list = async (req, res) => {
-
+    res.json(await Category.find({}).sort({ createdAt: -1 }).exec())
 }
 
 exports.read = async (req, res) => {
+    let category = await Category.findOne({ slug: req.params.slug }).exec();
+    res.json(category);
+};
 
-}
 
 exports.update = async (req, res) => {
-
+    const { name } = req.body
+    try {
+        const updated = await Category.findOneAndUpdate({ slug: req.params.slug },
+            { name, slug: slugify(name) }, { new: true })
+        res.json(updated)
+    } catch (err) {
+        res.status(400).send("Update category failed");
+    }
 }
 
 exports.remove = async (req, res) => {
-
-}
+    try {
+        const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+        res.json(deleted);
+    } catch (err) {
+        res.status(400).send("Delete category failed");
+    }
+};
