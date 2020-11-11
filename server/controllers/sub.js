fc9e54d58f7267dfd97ed8c1b77f21bea4b09e00
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-11 17:07:06 -0600

    implement sub category update

--- server/controllers/sub.js
+++ server/controllers/sub.js
@@ -24,10 +24,10 @@ exports.read = async (req, res) => {
 
 
 exports.update = async (req, res) => {
-    const { name } = req.body
+    const { name, parent } = req.body
     try {
         const updated = await Sub.findOneAndUpdate({ slug: req.params.slug },
-            { name, slug: slugify(name) }, { new: true })
+            { name, parent, slug: slugify(name) }, { new: true })
         res.json(updated)
     } catch (err) {
         res.status(400).send("Update sub failed");
