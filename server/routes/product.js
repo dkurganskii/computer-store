c9571697aa3cae025332d1fdc86645137fd4ef5a
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-02 10:18:34 -0600

    implement pagination

--- server/routes/product.js
+++ server/routes/product.js
@@ -9,12 +9,13 @@ const { create, listAll, remove, read, update, list, productsCount } = require("
 
 // routes
 router.post("/product", authCheck, adminCheck, create);
+router.get('/products/total', productsCount)
 router.get("/products/:count", listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
 
 router.post('/products', list)
-router.get('/products/total', productsCount)
+
 
 module.exports = router;
