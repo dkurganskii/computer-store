254926a42acdf8a706b1f525795335eb3c022c1d
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-06 06:34:45 -0600

    show related products

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -3,10 +3,13 @@ import { getProduct, productStar } from '../functions/product'
 import SingleProduct from '../components/cards/SingleProduct'
 import { useSelector } from 'react-redux'
 import { showAverage } from '../functions/rating'
+import { getRelated } from '../functions/product'
+import ProductCard from '../components/cards/ProductCard'
 
 const Product = ({ match }) => {
     const [product, setProduct] = useState({})
     const [star, setStar] = useState(0)
+    const [related, setRelated] = useState([])
 
     const { user } = useSelector((state) => ({ ...state }))
 
@@ -24,8 +27,13 @@ const Product = ({ match }) => {
         }
     })
 
-    const loadSingleProduct = () =>
-        getProduct(slug).then(res => setProduct(res.data))
+    const loadSingleProduct = () => {
+        getProduct(slug).then(res => {
+            setProduct(res.data)
+            // load related
+            getRelated(res.data._id).then(res => setRelated(res.data))
+        })
+    }
 
     const onStarClick = (newRating, name) => {
         setStar(newRating)
@@ -50,6 +58,12 @@ const Product = ({ match }) => {
                 <h4>Related Products</h4>
                 <hr />
             </div>
+        </div>
+        <div className='row pb-5'>
+            {related.length ? related.map((r) => (
+                <div key={r._id} className='col-md-4'>
+                    <ProductCard product={r} />
+                </div>)) : <div className='text-center col'>No Products Found</div>}
         </div>
     </div>
 }
