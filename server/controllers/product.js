7cbff0e0c8c42590622960903605b97b3d2a1a72
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-09 11:44:28 -0600

    implement filter by star rating - backend

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 const User = require("../models/user");
 const slugify = require("slugify");
-const { findById } = require("../models/product");
+const { findById, aggregate } = require("../models/product");
 
 exports.create = async (req, res) => {
     try {
@@ -201,8 +201,34 @@ const handleCategory = async (req, res, category) => {
     }
 }
 
+const handleStars = (req, res, stars) => {
+    Product.aggregate([
+        {
+            $project: {
+                document: "$$ROOT",
+                floorAverage: {
+                    $floor: { $avg: '$ratings.star' }
+                }
+            }
+        },
+        { $match: { floorAverage: stars } }
+    ])
+        .limit(12)
+        .exec((err, aggregates) => {
+            if (err) console.log('AGGREGATE ERROR', err)
+            Product.find({ _id: aggregates })
+                .populate('category', '_id name')
+                .populate('subs', '_id name')
+                .populate('postedBy', '_id name')
+                .exec((err, products) => {
+                    if (err) console.log('PRODUCT AGGREGATE ERROR', err)
+                    res.json(products)
+                })
+        })
+}
+
 exports.searchFilters = async (req, res) => {
-    const { query, price, category } = req.body
+    const { query, price, category, stars } = req.body
 
     if (query) {
         console.log('query', query)
@@ -218,5 +244,10 @@ exports.searchFilters = async (req, res) => {
     if (category) {
         console.log('category', category)
         await handleCategory(req, res, category)
+    }
+
+    if (stars) {
+        console.log('stars', category)
+        await handleStars(req, res, stars)
     }
 }
