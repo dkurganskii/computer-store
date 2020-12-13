53f37009dc69f07df4b056f1e360daf0db08c41c
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-13 08:44:19 -0600

    implement quantity update in cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 import { useDispatch } from 'react-redux'
 import ModalImage from 'react-modal-image'
 import laptop from "../../images/laptop.png";
@@ -26,6 +27,30 @@ const ProductCardInCheckout = ({ p }) => {
         }
     }
 
+    const handleQuantityChange = (e) => {
+        let count = e.target.value < 1 ? 1 : e.target.value
+        let cart = []
+        if (count > p.quantity) {
+            toast.error(`Max available quantity: ${p.quantity}`)
+            return
+        }
+        if (typeof window !== undefined) {
+            if (localStorage.getItem('cart')) {
+                cart = JSON.parse(localStorage.getItem('cart'))
+            }
+            cart.map((product, i) => {
+                if (product._id === p._id) {
+                    cart[i].count = count
+                }
+            })
+            localStorage.setItem('cart', JSON.stringify(cart))
+            dispatch({
+                type: 'ADD_TO_CART',
+                payload: cart
+            })
+        }
+    }
+
     return (
         <tbody>
             <tr>
@@ -41,11 +66,20 @@ const ProductCardInCheckout = ({ p }) => {
                 <td>{p.brand}</td>
                 <td>
                     <select onChange={handleColorChange} name='color' className='form-control'>
-                        {p.color ? <option value={p.color}>{p.color} </option> : <option>Select</option >}
-                        {colors.filter((c) => c !== p.color).map((c) => <option key={c} value={c}> {c}</option>)}
+                        {p.color ?
+                            <option value={p.color}>{p.color} </option> :
+                            <option>Select</option >}
+                        {colors.filter((c) => c !== p.color).map((c) =>
+                            <option key={c} value={c}> {c}</option>)}
                     </select>
                 </td>
-                <td>{p.count}</td>
+                <td className='text-center'>
+                    <input
+                        type='number'
+                        className='form-control'
+                        value={p.count}
+                        onChange={handleQuantityChange}></input>
+                </td>
                 <td>Shipping icon</td>
                 <td>Delete icon</td>
             </tr>
