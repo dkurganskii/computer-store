12343fb4dfab5259c0ecb438a054008adc525426
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-13 06:24:22 -0600

    show cart items in table

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import ProductCardInCheckout from '../components/cards/ProductCardInCheckout'
 
 const Cart = () => {
     const { cart, user } = useSelector((state) => ({ ...state }))
@@ -16,13 +17,34 @@ const Cart = () => {
 
     }
 
+    const showCartItems = () => (
+        <table className='table table-bordered'>
+            <thead className='thead-light'>
+                <tr>
+                    <th scope='col'>Image</th>
+                    <th scope='col'>Title</th>
+                    <th scope='col'>Price</th>
+                    <th scope='col'>Brand</th>
+                    <th scope='col'>Color</th>
+                    <th scope='col'>Count</th>
+                    <th scope='col'>Shipping</th>
+                    <th scope='col'>Remove</th>
+                </tr>
+            </thead>
+            {cart.map((p) => (
+                <ProductCardInCheckout key={p._id} p={p} />
+            ))}
+        </table>
+    )
+
     return (
         <div className='container-fluid pt-2'>
             <div className='row'>
                 <div className='col-md-8'>
                     <h4>Cart / {cart.length} Product</h4>
                     {!cart.length ? <p>No products in cart. <Link to='/shop'>Continue Shopping.</Link></p> :
-                        'Show cart items'}
+                        showCartItems()
+                    }
                 </div>
                 <div className='col-md-4'>
                     <h4>Order Summary</h4>
