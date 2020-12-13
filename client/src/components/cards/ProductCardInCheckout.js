23178e2e23c798a2f16fec465aa82c7fbfb1e314
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-13 06:42:05 -0600

    add react modal image

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import ModalImage from 'react-modal-image'
+import laptop from "../../images/laptop.png";
 
 const ProductCardInCheckout = ({ p }) => {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{ width: '100px', height: 'auto' }}>
+                        {p.images.length ?
+                            (<ModalImage small={p.images[0].url} large={p.images[0].url} />) :
+                            (<ModalImage small={laptop} large={laptop} />)}
+                    </div>
+                </td>
                 <td>{p.title}</td>
                 <td>${p.price}</td>
                 <td>{p.brand}</td>
