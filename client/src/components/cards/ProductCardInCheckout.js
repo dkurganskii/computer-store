04762155497667f49bbc51767f8c1a2c95b1b23b
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-13 07:32:54 -0600

    implement pick a color in cart page

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import ModalImage from 'react-modal-image'
 import laptop from "../../images/laptop.png";
 
 const ProductCardInCheckout = ({ p }) => {
+    const colors = ["Black", "Brown", "Silver", "White", "Blue"]
+    const dispatch = useDispatch()
+
+    const handleColorChange = (e) => {
+        let cart = []
+        if (typeof window !== undefined) {
+            if (localStorage.getItem('cart')) {
+                cart = JSON.parse(localStorage.getItem('cart'))
+            }
+            cart.map((product, i) => {
+                if (product._id === p._id) {
+                    cart[i].color = e.target.value
+                }
+            })
+            localStorage.setItem('cart', JSON.stringify(cart))
+            dispatch({
+                type: 'ADD_TO_CART',
+                payload: cart
+            })
+        }
+    }
+
     return (
         <tbody>
             <tr>
@@ -16,13 +39,18 @@ const ProductCardInCheckout = ({ p }) => {
                 <td>{p.title}</td>
                 <td>${p.price}</td>
                 <td>{p.brand}</td>
-                <td>{p.color}</td>
+                <td>
+                    <select onChange={handleColorChange} name='color' className='form-control'>
+                        {p.color ? <option value={p.color}>{p.color} </option> : <option>Select</option >}
+                        {colors.filter((c) => c !== p.color).map((c) => <option key={c} value={c}> {c}</option>)}
+                    </select>
+                </td>
                 <td>{p.count}</td>
                 <td>Shipping icon</td>
                 <td>Delete icon</td>
             </tr>
 
-        </tbody>
+        </tbody >
     )
 }
 
