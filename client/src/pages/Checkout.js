9790bd8e821c8504a5c61fe4fe66c06391680f61
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-18 11:44:00 -0600

    add coupon error and success message

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -12,7 +12,7 @@ const Checkout = () => {
     const [address, setAddress] = useState("");
     const [addressSaved, setAddressSaved] = useState(false);
     const [coupon, setCoupon] = useState('')
-    const [totalAfterDiscount, setTotalAfterDiscount] = useState('')
+    const [totalAfterDiscount, setTotalAfterDiscount] = useState(0)
     const [discountError, setDiscountError] = useState('')
 
     const dispatch = useDispatch();
@@ -40,6 +40,8 @@ const Checkout = () => {
         emptyUserCart(user.token).then((res) => {
             setProducts([]);
             setTotal(0);
+            setTotalAfterDiscount(0)
+            setCoupon('')
             toast.success("Cart is emapty. Contniue shopping.");
         });
     };
@@ -106,7 +108,10 @@ const Checkout = () => {
     const showApplyCoupon = () => (
         <>
             <input
-                onChange={e => setCoupon(e.target.value)}
+                onChange={e => {
+                    setCoupon(e.target.value)
+                    setDiscountError('')
+                }}
                 value={coupon}
                 type='text'
                 className='form-control' />
@@ -125,6 +130,8 @@ const Checkout = () => {
                 <h4>Got Coupon?</h4>
                 <br />
                 {showApplyCoupon()}
+                <br />
+                {discountError && <p className='bg-danger p-2'>{discountError}</p>}
             </div>
 
             <div className="col-md-6">
@@ -135,7 +142,9 @@ const Checkout = () => {
                 {showProductSummary()}
                 <hr />
                 <p>Cart Total: {total}</p>
-
+                {totalAfterDiscount > 0 && (
+                    <p className='bg-success p-2'>Discount Applied: Total Payable: ${totalAfterDiscount}</p>
+                )}
                 <div className="row">
                     <div className="col-md-6">
                         <button
