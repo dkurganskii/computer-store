7a9649e31ab655aa55d12e9fb2bbe0e79ff32f72
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-12-22 06:21:14 -0600

    show out of stock

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -68,9 +68,10 @@ const ProductCard = ({ product }) => {
                         <EyeOutlined className="text-warning" /> <br /> View Product
         </Link>,
                     <Tooltip title={tooltip}>
-                        <a onClick={handleAddToCart}>
-                            <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-                </a>
+                        <a onClick={handleAddToCart} disabled={product.quantity < 1}>
+                            <ShoppingCartOutlined className="text-danger" /> <br />
+                            {product.quantity > 1 ? 'Add to cart' : 'Out of stock'}
+                        </a>
                     </Tooltip>,
                 ]}
             >
